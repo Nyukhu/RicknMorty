a802dfcd4551ac7f5daf6f6fc243665a729d63b7
Adrien Guedes <guedesadrien@yahoo.fr>
2019-06-25 23:18:57 +0200

    base structure created

---
+++ src/components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-bar">
+    <h1>{{ "oui" }}</h1>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav-bar{
+    height: 5vh;
+    background-color: #161616;
+}
+</style>
---
+++ src/components/PageBody.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page-container">
+    <h1>{{ "oui" }}</h1>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'PageBody',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.page-container{
+    height: 100vh;
+    width: 100vw;
+    background-color: #2c2c2c;
+}
+</style>
